5779786258f22b066e16a97dfc88075d05813e40
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-04-14 20:02:37 +0700

    Frontend and backend

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.DB_STRING, {
+	useNewUrlParser : true,
+	useUnifiedTopology : true,
+});
+
+
+app.use(cors());
+app.use(express.json());
+app.use(authRoutes);
+app.listen(process.env.PORT, ()=>{
+	console.log(`Listening at port ${process.env.PORT}`);
+})
+
