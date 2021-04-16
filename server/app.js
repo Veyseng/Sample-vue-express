08747f1d6324c9d3aeee03cec0ae1894f6545a3b
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-04-16 15:08:19 +0700

    second edition

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const apiRoutes = require('./routes/api');
 
 require('dotenv').config();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.DB_STRING, {
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
+app.use(apiRoutes);
 app.listen(process.env.PORT, ()=>{
 	console.log(`Listening at port ${process.env.PORT}`);
 })
